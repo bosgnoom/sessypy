510b8a8812fd50f0b04eb64001d5bd340ffe0d2c
bosgnoom <paul.schouten@gmail.com>
2024-04-01 20:28:16 +0200

    version check included in example.py

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,7 @@
 import asyncio
 from config import * # Hide your secrets in example.py
 from sessypy.devices import SessyDevice, SessyP1Meter, SessyBattery, get_sessy_device
+import pprint
 
 async def run():
     devices = list()
@@ -17,39 +18,41 @@ async def run():
         print(f"S/N: {device.serial_number}")
         print("- Network status -")
         result = await device.get_network_status()
-        print(result)
+        pprint.pprint(result)
         print("")
 
         print("- Software update status -")
-        result = await device.get_ota_status()
-        print(result)
+        ota_result = await device.get_ota_status()
+        pprint.pprint(ota_result)
         print("")
 
         if isinstance(device, SessyBattery):
             print("- Power Status -")
             result = await device.get_power_status()
-            print(result)
+            pprint.pprint(result)
             print("")
 
             print("- Power Strategy -")
             result = await device.get_power_strategy()
-            print(result)
+            pprint.pprint(result)
             print("")
 
             print("- System settings -")
             result = await device.get_system_settings()
-            print(result)
+            pprint.pprint(result)
             print("")
 
-            print("- Dynamic mode schedule -")
-            result = await device.get_dynamic_schedule()
-            print(result)
-            print("")
+            if ota_result['self']['installed_firmware']['version'] >= '1.6.5':
+                # Dynamic mode from 1.6.5 and up
+                print("- Dynamic mode schedule -")
+                result = await device.get_dynamic_schedule()
+                pprint.pprint(result)
+                print("")
 
         elif isinstance(device, SessyP1Meter):
             print("- P1 Status -")
             result = await device.get_p1_status()
-            print(result)
+            pprint.pprint(result)
             print("")
 
         await device.close()
